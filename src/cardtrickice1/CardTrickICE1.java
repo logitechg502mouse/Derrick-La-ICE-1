ff6598c4e8b84440ddc2bbbaf2a36f1e64ad46a2
logitechg502mouse <lader@sheridancollege.ca>
2022-01-27 23:51:59 -0500

    version B 2.0, inputted scanner for user inputs, really rough idea

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package cardtrickice1;
+import java.util.Scanner;
 
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
@@ -20,13 +21,21 @@ public class CardTrickICE1 {
      */
     public static void main(String[] args) 
     {
-        Card[] magicHand = new Card[7]; //Array of object
-        for( int i=0;i<magicHand.length;i++)
-        {
-            Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
-            c1.setSuits("hearts");//random method suit 
-        }
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter the card's suit: ");
+        String userSuit = userInput.next();
+        int userCard;
+        System.out.println("Please enter the card number form 1 to 13:");
+        userCard = userInput.nextInt();
+        
+        System.out.println(userSuit +" card number: " + userCard);
+   //     Card[] magicHand = new Card[7]; //Array of object
+      //  for( int i=0;i<magicHand.length;i++)
+       // {
+      //      Card c1 = new Card();
+      //      c1.setValue(2);//use a method to generate random *13
+      //      c1.setSuits("hearts");//random method suit 
+      //  }
         //step 2:take input 
         
         //step 3: match with array 
