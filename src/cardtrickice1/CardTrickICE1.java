2ae1339d06c84abc51697208f04533238394416b
logitechg502mouse <lader@sheridancollege.ca>
2022-01-30 23:39:00 -0500

    Version B 4.2 CardTrick,rearranged source code to be more readable

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -23,6 +23,12 @@ public class CardTrickICE1 {
     {
       
         
+         Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter the card's suit (hearts, spades, diamonds, clubs): ");
+        String userSuit = userInput.next().toLowerCase();      
+        System.out.println("Please enter the card number form 1 to 13:");
+        int userCard = userInput.nextInt();
+        
         Card[] magicHand = new Card[7];
         for(int i = 0; i < magicHand.length; i++){
             Card Cards = new Card();
@@ -31,28 +37,23 @@ public class CardTrickICE1 {
             Cards.setValue(Cards.generateCardNum());            
             magicHand[i] = Cards;                   
         }
-         for(int i = 0; i< magicHand.length; i++){
-            System.out.println(magicHand[i].getValue() +" " + magicHand[i].getSuits());     
+        System.out.println("\n------------Cards Generated------------");
+        for(int i = 0; i< magicHand.length; i++){
+            System.out.println(magicHand[i].getValue() +" of " + magicHand[i].getSuits());     
         }
          
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter the card's suit: ");
-        String userSuit = userInput.next().toLowerCase();      
-        System.out.println("Please enter the card number form 1 to 13:");
-        int userCard = userInput.nextInt();
         boolean inHand = false;
         for(int i = 0; i< magicHand.length; i++){
            if(userCard == magicHand[i].getValue() && userSuit.equalsIgnoreCase(magicHand[i].getSuits())){
                inHand = true;
-           }
-            
+           }         
         }
+        
         if(true == inHand){
-            System.out.println("Great! your card, "+ userCard + " of " + userSuit + " is in this hand.");
-            
+            System.out.println("\nGreat! your card, "+ userCard + " of " + userSuit + ", is in this hand.");      
         }
         else{
-            System.out.println("Sorry. Your card is not in this hand, better luck next time.");
+            System.out.println("\nSorry. Your card is not in this hand, better luck next time.");
         }
         
         
